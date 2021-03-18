e9ade4f422dd9cc783ccca5e886b21cde1b7995b
ghinahanifah <ghinahaniffah@gmail.com>
2021-03-18 23:18:39 +0800

    tampilanawal

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index(){
+        $title="Data Mahasiswa";
+        $data['mahasiswa']=array(
+            'nim'=>'1905021004',
+            'nama'=>'Najah Ghina Nur hanifah ',
+            'alamat'=>'Banyuning',
+            'nohp'=>'08983248030',
+            'email'=>'najah@undiksha.ac.id'
+        );
+        return view('admin.beranda', compact('title','data'));
+    }
+    public function dashboard(){
+        $title="Data Dashboard";
+
+        return view('admin.dashboard', compact('title'));
+    }
+}
